c095d81884d7137e47884aa6eb26cabc126775ed
L-liang <liangliang8768@qq.com>
2015-11-12 16:26:21 +0800

    自定义分页注解

---
+++ boya-core/src/main/java/com/github/boyaframework/core/spring/PageConfig.java
@@ -0,0 +1,20 @@
+package com.github.boyaframework.core.spring;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PageConfig {
+	/** 
+	 * @Title: pageable 
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @return boolean    返回类型 
+	 * @throws 
+	 */
+	boolean pageable() default true;
+}
---
+++ boya-core/src/main/java/com/github/boyaframework/core/spring/PageInterceptor.java
@@ -0,0 +1,52 @@
+package com.github.boyaframework.core.spring;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import com.github.boyaframework.core.persistence.Page;
+import com.github.pagehelper.PageHelper;
+
+
+/**
+ * 分页的web层拦截器
+ * 
+ * @author Boya
+ */
+public class PageInterceptor {
+	public Object doBasicProfiling(ProceedingJoinPoint pjp) throws Throwable {
+		Page<?> page = null;
+		Object[] args =  pjp.getArgs();
+		boolean pageable = true;
+		for (Object arg : args) {
+			if (arg instanceof Page) {
+				Method method = ((MethodSignature)pjp.getSignature()).getMethod();
+				Annotation[][] ass = method.getParameterAnnotations();
+				for (int i = 0; i < ass.length; i++) {
+					for (int j = 0; j < ass[i].length; j++) {
+						Annotation a = ass[i][j];
+						if(a instanceof PageConfig) {
+							PageConfig pageConfig = (PageConfig) a;
+							pageable = pageConfig.pageable();
+						}
+					}
+				}
+				if (pageable)
+					page = (Page<?>)arg;
+				break;
+			}
+		}
+		if (page != null) {
+			PageHelper.startPage(page.getPageNo(), page.getPageSize());
+			if (StringUtils.isNotBlank(page.getSortStr())) {
+				PageHelper.orderBy(page.getSortStr());
+			}
+		}
+		Object object = pjp.proceed();// 执行该方法
+		return object;
+	}
+
+}
