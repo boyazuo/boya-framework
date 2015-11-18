a72cb0003c5a9ea60caf74660fe735f44ae078ea
L-liang <liangliang8768@qq.com>
2015-11-18 22:22:45 +0800

    修改 appid 和 appsecret 为自己的测试账号

--- boya-wechat/src/main/java/com/github/boyaframework/wechat/contants/BaseContants.java
+++ boya-wechat/src/main/java/com/github/boyaframework/wechat/contants/BaseContants.java
@@ -2,7 +2,7 @@ package com.github.boyaframework.wechat.contants;
 
 public class BaseContants {
 	
-	public static final String APPID = "123";
+	public static final String APPID = "wx63535708a8a49faf";
 	
-	public static final String APPSECRET = "132";
+	public static final String APPSECRET = "712e6c6d561009d9de4d72ed9cd66d56";
 }
