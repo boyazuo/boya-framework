1cb7e026269be2ea90f97eb1187cfafa52565039
boyazuo <6989006@qq.com>
2015-11-22 16:54:09 +0800

    代码调整

--- boya-core/src/main/java/com/github/boyaframework/core/spring/SpringContextHolder.java
+++ boya-core/src/main/java/com/github/boyaframework/core/spring/SpringContextHolder.java
@@ -10,6 +10,7 @@ import org.springframework.context.ApplicationContextAware;
 /**
  * 以静态变量保存Spring ApplicationContext, 可在任何代码任何地方任何时候取出ApplicaitonContext.
  * 
+ * @author calvin
  */
 public class SpringContextHolder implements ApplicationContextAware, DisposableBean {
 
--- boya-core/src/main/java/com/github/boyaframework/core/utils/Identities.java
+++ boya-core/src/main/java/com/github/boyaframework/core/utils/Identities.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 /**
  * 封装各种生成唯一性ID算法的工具类.
  * 
+ * @author calvin
  */
 public class Identities {
 
