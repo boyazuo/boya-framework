2099974545e64ad79dd8ff3007bfcddf2369ae8c
L-liang <liangliang8768@qq.com>
2015-12-01 17:38:26 +0800

    从微信服务器下载多媒体文件接口

---
+++ boya-wechat/src/main/java/com/github/boyaframework/wechat/utils/WxMediaUtils.java
@@ -0,0 +1,25 @@
+package com.github.boyaframework.wechat.utils;
+
+public class WxMediaUtils {
+	/** 
+	 * @Title: getExe 
+	 * @Description: 通过 contentType得到 文件类型
+	 * @param contentType
+	 * @return String    返回类型 
+	 * @throws 
+	 */
+	public static String getExe(String contentType) {
+		switch (contentType) {
+		case "image/jpeg":
+			return ".jpg";
+		case "application/x-png":
+			return ".png";
+		case "image/gif":
+			return ".gif";
+		case "application/x-bmp":
+			return ".bmp";
+		default:
+			return "";
+		}
+	}
+}
