b739f4c94f999597401ed45eed154c85df8681f5
boyazuo <6989006@qq.com>
2015-12-17 10:57:40 +0800

    添加IdEntity

---
+++ boya-core/src/main/java/com/github/boyaframework/core/persistence/IdEntity.java
@@ -0,0 +1,18 @@
+package com.github.boyaframework.core.persistence;
+
+/**
+ * 通用实体基类
+ * 
+ * @author Boya
+ *
+ */
+public class IdEntity {
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
